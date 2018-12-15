4e6b90cb8c0ea1a892058cefacf627a4726ba646
Myung Kim <mjk287@cornell.edu>
2018-12-15 01:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHoldiaysto(name){
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidayto(holiday, name){
+  return "Happy ${holiday}, ${name}"
+}
+
+function h
